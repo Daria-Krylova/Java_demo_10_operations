52eec3f5a7c2c2904897a805ab3f5087038f2a04
Daria-Krylova <dalidass@mail.ru>
2024-01-26 03:02:28 +0300

    Initial commit

---
+++ src/main/java/com/example/demo_10_operations/Demo10OperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_10_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo10OperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo10OperationsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo_10_operations/MathOperationsProgram.java
@@ -0,0 +1,2 @@
+package com.example.demo_10_operations;public class MathOperationsProgram {
+}
